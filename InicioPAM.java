e2e157575eb942db90ecbf38981d170c0c094d6e
Jorge <124047199@upq.edu.mx>
2025-09-04 14:35:53 -0600

    Create InicioPAM.java

---
+++ InicioPAM.java
@@ -0,0 +1,34 @@
+package inicio;
+
+public class InicioPAM {
+    
+    public String reglamentoPOO() {
+        return "Reglamento POO:\n"
+             + "1. Asistencia mínima del 80%\n"
+             + "2. Respeto en clase\n"
+             + "3. Entrega puntual de tareas\n"
+             + "4. Participación activa";
+    }
+
+    public String lineamientosClassroom() {
+        return "Lineamientos Classroom:\n"
+             + "- Subir tareas en formato PDF\n"
+             + "- Cumplir con las fechas límite\n"
+             + "- Revisar anuncios diariamente\n"
+             + "- Evitar plagio en actividades";
+    }
+
+    public String fechasDeParciales() {
+        return "Fechas de Parciales:\n"
+             + "Parcial 1: 20 de Septiembre\n"
+             + "Parcial 2: 25 de Octubre\n"
+             + "Parcial 3: 30 de Noviembre";
+    }
+
+    public String porcentajesPorParcial() {
+        return "Porcentajes por Parcial:\n"
+             + "Parcial 1: 30%\n"
+             + "Parcial 2: 30%\n"
+             + "Parcial 3: 40%";
+    }
+}
