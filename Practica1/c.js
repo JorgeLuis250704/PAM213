4ac9efcaf512111d16b87e5fd10542c1d97ef9df
Jorge <124047199@upq.edu.mx>
2025-09-17 20:34:37 -0600

    Create c.js

---
+++ Practica1/c.js
@@ -0,0 +1,8 @@
+// La función recibe 'nombre' y 'edad' y retorna un saludo personalizado.
+const saludoPersonalizado = (nombre, edad) => `Hola, me llamo ${nombre} y tengo ${edad} años.`;
+
+// Ejemplo de uso de la función con los datos del ejercicio.
+const mensaje = saludoPersonalizado("Jorge Luis", 21);
+
+// Se muestra el resultado en la consola.
+console.log(mensaje); // Resultado: "Hola, me llamo Jorge y tengo 21 años."
