62d11ec510c0687cbbeee04c353236d806ca0d38
JorgeLuis250704 <124047199@upq.edu.mx>
2025-09-23 16:08:41 -0600

    update
    last week

---
+++ Practica1/b.js
@@ -0,0 +1,7 @@
+// Se define la función 'cuadrado' usando la sintaxis de arrow function.
+const cuadrado = (numero) => numero * numero;
+
+// Se prueba la función con 3 números diferentes y se muestra en consola.
+console.log(`El cuadrado de 2 es: ${cuadrado(2)}`); // Resultado: 4
+console.log(`El cuadrado de 5 es: ${cuadrado(5)}`); // Resultado: 25
+console.log(`El cuadrado de 10 es: ${cuadrado(10)}`); // Resultado: 100
