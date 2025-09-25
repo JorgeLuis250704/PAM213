d0a293c9d912cfbfb4c3e3131b901a9f3a6a39ba
JorgeLuis250704 <124047199@upq.edu.mx>
2025-09-25 14:49:11 -0600

    practica 3 a.js

---
+++ Practica3/a/main.js
@@ -0,0 +1,8 @@
+// Importa la función 'restar' desde el módulo utils.js
+import { restar } from './utils.js';
+
+// Ejecutar varias pruebas
+console.log('Prueba 1: 5 - 3 =', restar(5, 3));      // Esperado: 2
+console.log('Prueba 2: 10 - 7 =', restar(10, 7));    // Esperado: 3
+console.log('Prueba 3: 0 - 5 =', restar(0, 5));      // Esperado: -5
+console.log('Prueba 4: -2 - 8 =', restar(-2, 8));    // Esperado: -10
---
+++ Practica3/a/utils.js
@@ -0,0 +1,9 @@
+/**
+ * Retorna la resta de dos números.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number} La resta de a y b.
+ */
+export function restar(a, b) {
+  return a - b;
+}
